4ced58413ab7cedf1d34d2b24a28518dc77a0d01
Nate Snyder <n8.snyder@berkeley.edu>
2022-04-13 11:06:27 -0700

    Renamed binary-tree.test and solved sumValues

--- tree.js
+++ tree.js
@@ -15,7 +15,20 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    if (this.root === null) return 0;
+
+    const unvisited = [this.root];
+    let total = 0;
+
+    while (unvisited.length !== 0) {
+      let node = unvisited.pop();
+      total += node.val;
+
+      for (let child of node.children) {
+        unvisited.push(child);
+      }
+    }
+    return total;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
